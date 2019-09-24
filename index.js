3782838eeae2049b29140519c5e1bfd4ce018fde
Valeriya Zavozova <leriya.15@mail.ru>
2019-09-24 18:19:31 +0300

    Separated events added

--- index.js
+++ index.js
@@ -2,39 +2,41 @@
 class ToDoRender {
 
   createItemRender(value){
-     let newToDo = document.createElement("LI");
-     newToDo.innerHTML = value;
+    let newToDo = document.createElement("LI");
+    newToDo.innerHTML = value;
      
-     let check = document.createElement("input");
-     check.type = "checkbox";
-     check.className = "checkbox";
-     check.value = value;
+    let check = document.createElement("input");
+    check.type = "checkbox";
+    check.className = "checkbox";
+    check.value = value;
  
-     let del = document.createElement("button");
-     del.value = value;
-     del.innerHTML = "&#10008";
-     del.className = "clear";
+    let del = document.createElement("button");
+    del.value = value;
+    del.innerHTML = "&#10008";
+    del.className = "clear";
+    //del.onclick = function() {deleteElement(del);};
      
-     newToDo.appendChild(check);
-     newToDo.appendChild(del);
-     let list = document.querySelector('div');
-     list.appendChild(newToDo);
-     return [del,check];      
-   }   
+    newToDo.appendChild(check);
+    newToDo.appendChild(del);
+    let list = document.querySelector('div');
+    list.appendChild(newToDo);
+    return [del,check];      
+  }   
 
   itemDone(element) { 
-             element.parentElement.style.textDecoration = "line-through";
-             element.parentElement.style.color = "#a79a9afd"; 
-      }          
+    element.parentElement.style.textDecoration = "line-through";
+    element.parentElement.style.color = "#a79a9afd"; 
+  }
+
   itemUndone(element) { 
-          element.parentElement.style.textDecoration = "";
-          element.parentElement.style.color = "";
-   }       
+    element.parentElement.style.textDecoration = "";
+    element.parentElement.style.color = "";
+  }       
    
 
-   clearRender(element) {
-       element.parentElement.remove()
-   }
+  clearRender(element) {
+    element.parentElement.remove()
+  }
 }
 
 class ToDoList {
@@ -49,9 +51,7 @@ class ToDoList {
     newNode.next = this.head;
     newNode.done = false;
     this.head = newNode;
-    this.lenght++;
-    
-    this.createItem(value);
+    this.lenght++;    
   }
 
   addToEnd(value){
@@ -71,26 +71,26 @@ class ToDoList {
   }  
   
   createItem(value) {        
-      const [del, check] = ToDoRender.createItemRender(value);
-      console.log(this);
-      this.clear(del);
-      this.done(check);        
-      return this;
+    const [del, check] = ToDoRender.createItemRender(value);
+    console.log(this);
+    this.clear(del);
+    this.done(check);        
+    return this;
   }
 
   done(element) {
-  const value = element.value;
-  let Node = this.head;
+    const value = element.value;
+    let Node = this.head;
 
-  while (Node){
-    if (Node.value === value)
-    break;
-    Node = Node.next;}     
-
+    while (Node){
+      if (Node.value === value)
+      break;
+      Node = Node.next;
+    }
     element.onchange = () => {             
       if (element.checked){                   
-          Node.done = true;
-          ToDoRender.itemDone(element); 
+        Node.done = true;
+        ToDoRender.itemDone(element); 
       }
       else {            
         Node.done = false;
@@ -144,18 +144,28 @@ class ToDoList {
 let buttonAdd = document.getElementById("Create");
 let input = document.querySelector("input");
   
-input.onkeyup = () => {
+function CheckInput() {
   if (input.value==="") buttonAdd.disabled = true;
   else  buttonAdd.disabled = false;
 }
-  
-buttonAdd.onclick = () => {                
-  ToDo1.addToHead(input.value);      
+
+function AddNew() {                
+  ToDo1.addToHead(input.value);
+  ToDo1.createItem(input.value);      
   input.value='';
   buttonAdd.disabled = true;
+}
+
+function deleteElement(element) {
+  let value = element.value;    
+  ToDo1.removeItem(value);
+  //ToDo1.clearRender(element);        
+  console.log(ToDo1)
 }
 
 let ToDo1 = new ToDoList();  
 console.log(ToDo1);
 ToDo1.addToHead('first');
+ToDo1.createItem('first');
 ToDo1.addToHead('second');
+ToDo1.createItem('second');
